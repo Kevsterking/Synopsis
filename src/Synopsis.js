b76cff309f7cf49d2f23706fa6f2f449b7199a5c
unknown <kevin.backstade@gmail.com>
2023-08-07 14:03:24 +0200

    diagram grid lines added

--- src/Synopsis.js
+++ src/Synopsis.js
@@ -2,7 +2,7 @@ function Synopsis() {
   return <div id="diagram-root" style={{position: "relative", width: "1500px", height: "900px", backgroundColor: "rgb(51, 51, 51)", overflow: "hidden"}}>
     <canvas id="diagram-canvas" style={{position: "absolute", width: "100%", height: "100%"}}>
     </canvas>
-    <div id="diagram-elements" style={{position: "absolute", border: "2px solid yellow", width: "100px", height: "100px", left: "50%", top: "50%"}}>
+    <div id="diagram-elements" style={{position: "absolute", border: "1px solid white", transform: "translate(-50%, -50%)", width: "100px", height: "100px", left: "50%", top: "50%"}}>
     </div>
   </div> 
 }
@@ -12,6 +12,10 @@ function Diagram(root_element, canvas_element, elements_element) {
   this.root_element = root_element;
   this.canvas_element = canvas_element;
   this.elements_element = elements_element;
+
+  this.canvasContext = this.canvas_element.getContext("2d");
+  this.canvasContext.canvas.width  = this.canvas_element.offsetWidth;
+  this.canvasContext.canvas.height = this.canvas_element.offsetHeight;
 
   this.translation = { x: 0, y: 0 };
   this.scale = 1;
@@ -33,16 +37,62 @@ function Diagram(root_element, canvas_element, elements_element) {
     }
   }
 
-  this.setTranslation = (x, y) => {
-    this.translation.x = Math.max(x, - this.root_element.clientWidth * 0.5 - this.elements_element.offsetWidth);
-    this.translation.y = Math.max(y, - this.root_element.clientHeight * 0.5 - this.elements_element.offsetHeight);
-    this.translation.x = Math.min(this.translation.x, this.root_element.clientWidth * 0.5);
-    this.translation.y = Math.min(this.translation.y, this.root_element.clientHeight * 0.5);
-    this.elements_element.style.transform = "translate("+this.translation.x+"px, "+this.translation.y+"px)";
+  /*
+    Update the background grid on which the diagram is shown
+  */
+  this.updateGrid = () => {
+    
+    this.canvasContext.clearRect(0, 0, this.canvas_element.offsetWidth, this.canvas_element.offsetHeight);
+
+    this.canvasContext.beginPath();
+
+    this.canvasContext.strokeStyle = "rgb(45, 45, 45)";
+    this.canvasContext.lineWidth = 0.1;
+
+    for (let x = (this.translation.x - this.canvas_element.offsetWidth) % 50; x < this.canvas_element.offsetWidth; x += 50) {
+      this.canvasContext.moveTo(x, 0);
+      this.canvasContext.lineTo(x, this.canvas_element.offsetHeight);
+    }
+    for (let y = (this.translation.y - this.canvas_element.offsetHeight) % 50; y < this.canvas_element.offsetWidth; y += 50) {
+      this.canvasContext.moveTo(0, y);
+      this.canvasContext.lineTo(this.canvas_element.offsetWidth, y);
+    }
+
+    this.canvasContext.strokeStyle = "rgb(70, 70, 70)";
+    this.canvasContext.lineWidth = 0.5;
+    
+    for (let x = (this.translation.x - this.canvas_element.offsetWidth) % 100; x < this.canvas_element.offsetWidth; x += 100) {
+      this.canvasContext.moveTo(x, 0);
+      this.canvasContext.lineTo(x, this.canvas_element.offsetHeight);
+    }
+    for (let y = (this.translation.y - this.canvas_element.offsetHeight) % 100; y < this.canvas_element.offsetWidth; y += 100) {
+      this.canvasContext.moveTo(0, y);
+      this.canvasContext.lineTo(this.canvas_element.offsetWidth, y);
+    }
+    
+    this.canvasContext.stroke();
+
   }
 
-  this.setScale = () => {
-    console.log("set scale now");
+  /*
+    Translate the panzoom area to (x, y) 
+  */
+  this.setTranslation = (x, y) => {
+    
+    this.translation.x = Math.max(x, - (this.root_element.clientWidth + this.elements_element.offsetWidth) * 0.5 + this.root_element.clientWidth * 0.1);
+    this.translation.y = Math.max(y, - (this.root_element.clientHeight + this.elements_element.offsetHeight) * 0.5 + this.root_element.clientHeight * 0.1);
+    this.translation.x = Math.min(this.translation.x, this.root_element.clientWidth * 0.5 - this.root_element.clientWidth * 0.1);
+    this.translation.y = Math.min(this.translation.y, this.root_element.clientHeight * 0.5 - this.root_element.clientHeight * 0.1);
+    
+    this.updateGrid();
+    this.elements_element.style.transform = "translate(-50%, -50%) translate("+this.translation.x+"px, "+this.translation.y+"px) scale("+this.scale+")";
+    
+  }
+
+  this.setScale = (scale) => {
+    this.scale = scale;
+    this.elements_element.style.transform = "translate(-50%, -50%) translate("+this.translation.x+"px, "+this.translation.y+"px) scale("+this.scale+")";
+    this.updateGrid();
   }
 
   this.root_element.onmousedown = (e) => {
@@ -78,6 +128,10 @@ function Diagram(root_element, canvas_element, elements_element) {
   this.root_element.onmousemove = mouseMoveAction;
 
   this.root_element.onmouseup = mouseUpAction;
+
+  this.root_element.onwheel = (e) => {
+    this.setScale();
+  }
 
 }
 
