2200daefcf14180af3805615bfb6d85367e11e8a
unknown <kevin.backstade@gmail.com>
2023-08-07 18:58:59 +0200

    work on placement and bounds of element box, aswell as added a todo tracker and a devlog

--- src/Synopsis.js
+++ src/Synopsis.js
@@ -2,12 +2,12 @@ function Synopsis() {
   return <div id="diagram-root" style={{position: "relative", width: "1500px", height: "900px", backgroundColor: "rgb(51, 51, 51)", overflow: "hidden"}}>
     <canvas id="diagram-canvas" style={{position: "absolute", width: "100%", height: "100%"}}>
     </canvas>
-    <div id="diagram-elements" style={{position: "absolute", border: "1px solid white", transform: "translate(-100%, -100%)", width: "100px", height: "100px", left: "50%", top: "50%"}}>
+    <div id="diagram-elements" style={{position: "absolute", border: "1px solid white", width: "300px", height: "300px", left: "50%", top: "50%"}}>
     </div>
   </div> 
 }
 
-function Diagram(root_element, canvas_element, elements_element) {
+function Diagram(root_element, canvas_element, elements_element, left_corner_coordinate) {
   
   this.root_element = root_element;
   this.canvas_element = canvas_element;
@@ -17,7 +17,8 @@ function Diagram(root_element, canvas_element, elements_element) {
   this.canvasContext.canvas.width  = this.canvas_element.offsetWidth;
   this.canvasContext.canvas.height = this.canvas_element.offsetHeight;
 
-  this.translation = { x: 0, y: 0 };
+  this.offset = { x: left_corner_coordinate.x, y: left_corner_coordinate.y };
+  this.translation = { x: null, y: null };
   
   this.mouse_down = { translation: null, is_down: false, x: null, y: null };
   this.delta = { x: 0, y: 0 };
@@ -48,11 +49,11 @@ function Diagram(root_element, canvas_element, elements_element) {
 
     this.canvasContext.beginPath();
 
-    for (let x = (this.translation.x - this.canvas_element.offsetWidth) % 100 - 50; x < this.canvas_element.offsetWidth; x += 100) {
+    for (let x = (this.translation.x - this.canvas_element.offsetWidth * 0.5) % 100 - 50; x < this.canvas_element.offsetWidth; x += 100) {
       this.canvasContext.moveTo(x, 0);
       this.canvasContext.lineTo(x, this.canvas_element.offsetHeight);
     }
-    for (let y = (this.translation.y - this.canvas_element.offsetHeight) % 100 - 50; y < this.canvas_element.offsetWidth; y += 100) {
+    for (let y = (this.translation.y - this.canvas_element.offsetHeight * 0.5) % 100 - 50; y < this.canvas_element.offsetWidth; y += 100) {
       this.canvasContext.moveTo(0, y);
       this.canvasContext.lineTo(this.canvas_element.offsetWidth, y);
     }
@@ -65,15 +66,18 @@ function Diagram(root_element, canvas_element, elements_element) {
     this.canvasContext.strokeStyle = "rgb(80, 80, 80)";
     this.canvasContext.lineWidth = 1;
     
-    for (let x = (this.translation.x - this.canvas_element.offsetWidth) % 100; x < this.canvas_element.offsetWidth; x += 100) {
+    for (let x = (this.translation.x - this.canvas_element.offsetWidth * 0.5) % 100; x < this.canvas_element.offsetWidth; x += 100) {
       this.canvasContext.moveTo(x, 0);
       this.canvasContext.lineTo(x, this.canvas_element.offsetHeight);
     }
-    for (let y = (this.translation.y - this.canvas_element.offsetHeight) % 100; y < this.canvas_element.offsetWidth; y += 100) {
+    for (let y = (this.translation.y - this.canvas_element.offsetHeight * 0.5) % 100; y < this.canvas_element.offsetWidth; y += 100) {
       this.canvasContext.moveTo(0, y);
       this.canvasContext.lineTo(this.canvas_element.offsetWidth, y);
     }
     
+    //this.canvasContext.moveTo((this.translation.x - this.canvas_element.offsetWidth * 0.5), 0);
+    //this.canvasContext.lineTo(x, this.canvas_element.offsetHeight);
+   
     this.canvasContext.closePath();
     this.canvasContext.stroke();
 
@@ -84,20 +88,14 @@ function Diagram(root_element, canvas_element, elements_element) {
   */
   this.setTranslation = (x, y) => {
     
-    this.translation.x = Math.max(x, - (this.root_element.clientWidth + this.elements_element.offsetWidth) * 0.5 + this.root_element.clientWidth * 0.1);
-    this.translation.y = Math.max(y, - (this.root_element.clientHeight + this.elements_element.offsetHeight) * 0.5 + this.root_element.clientHeight * 0.1);
-    this.translation.x = Math.min(this.translation.x, this.root_element.clientWidth * 0.5 - this.root_element.clientWidth * 0.1);
-    this.translation.y = Math.min(this.translation.y, this.root_element.clientHeight * 0.5 - this.root_element.clientHeight * 0.1);
+    this.translation.x = Math.max(x, - this.offset.x - this.root_element.clientWidth * 0.5 - this.elements_element.offsetWidth + 100);
+    this.translation.y = Math.max(y, - this.offset.y - this.root_element.clientHeight * 0.5 - this.elements_element.offsetHeight + 100);
+    this.translation.x = Math.min(this.translation.x,  this.root_element.clientWidth * 0.5 - 100 - this.offset.x);
+    this.translation.y = Math.min(this.translation.y,  this.root_element.clientHeight * 0.5 - 100 - this.offset.y);
     
     this.updateGrid();
-    this.elements_element.style.transform = "translate(-100%, -100%) translate("+this.translation.x+"px, "+this.translation.y+"px) scale("+this.scale+")";
+    this.elements_element.style.transform = "translate("+this.offset.x+"px, "+this.offset.y+"px) translate("+this.translation.x+"px, "+this.translation.y+"px)";
     
-  }
-
-  this.setScale = (scale) => {
-    this.scale = scale;
-    this.elements_element.style.transform = "translate(-100%, -100%) translate("+this.translation.x+"px, "+this.translation.y+"px) scale("+this.scale+")";
-    this.updateGrid();
   }
 
   this.root_element.onmousedown = (e) => {
@@ -134,11 +132,7 @@ function Diagram(root_element, canvas_element, elements_element) {
 
   this.root_element.onmouseup = mouseUpAction;
 
-  this.root_element.onwheel = (e) => {
-    this.setScale(1);
-  }
-
-  this.updateGrid();
+  this.setTranslation(0, 0);
 
 }
 
@@ -148,7 +142,7 @@ window.onload = (event) => {
   const diagram_canvas = document.getElementById("diagram-canvas");
   const diagram_elements = document.getElementById("diagram-elements");
 
-  let dia = new Diagram(diagram_root, diagram_canvas, diagram_elements);
+  let dia = new Diagram(diagram_root, diagram_canvas, diagram_elements, {x: 0, y: 0});
 
 };
 
