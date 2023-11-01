8c72f5c0eac78801fdde66b1caf13e0df8b3852e
kevsterking <kevin.backstade@gmail.com>
2023-11-01 05:23:35 +0100

    fixed resize tranlation issue

--- src/core/controller/SynopsisDiagramController.js
+++ src/core/controller/SynopsisDiagramController.js
@@ -23,45 +23,19 @@ function SynopsisDiagramController() {
     this.diagram.set_translation(pos.x, pos.y);
   }
 
-  const update_scroll_extent = () => {
-    const cx = this.diagram.dom.content.clientWidth * 0.5;
-    const cy = this.diagram.dom.content.clientHeight * 0.5;
-    this.scroller.extent.x.min = this.diagram.content.extent.x.min - this.diagram.padding.x + cx;
-    this.scroller.extent.x.max = this.diagram.content.extent.x.max + this.diagram.padding.x - cx;
-    this.scroller.extent.y.min = this.diagram.content.extent.y.min - this.diagram.padding.y + cy;
-    this.scroller.extent.y.max = this.diagram.content.extent.y.max + this.diagram.padding.y - cy;
-  }
-
-  const unbind_content = () => {
-    this.content?.on_extent_change.unsubscribe(update_scroll_extent);
-    this.content = null;
-  }
-
-  const bind_content = content => {
-    unbind_content();
-    this.content = content;
-    this.content.on_extent_change.subscribe(update_scroll_extent); 
-    update_scroll_extent();
-  }
-
-  const on_set_content = content => {
-    if (content?.spawned) {
-      bind_content(content);
-    } else {
-      content?.on_load.subscribe(bind_content);
-    }
+  const diagram_resize = () => {
+    this.scroller.set_position_no_event(this.diagram.translation.x, this.diagram.translation.y);
   }
 
   const bind = diagram => {
     this.diagram = diagram;
     this.scroller.bind(this.diagram.dom.root, on_scroll, this.diagram.extent);
-    this.diagram.on_set_content.subscribe(on_set_content); 
+    this.diagram.on_resize.subscribe(diagram_resize);
   }
 
   const unbind = () => {
-    this.diagram.on_set_content.unsubscribe(on_set_content);
-    unbind_content();
     this.scroller.unbind();
+    this.diagram.on_resize.unsubscribe(diagram_resize);
     this.diagram = null;
   }
 
